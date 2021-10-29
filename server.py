7a4d5f7e79c5ea63474e05efae09e4ce2e5477f9
Tóth-Vargics Noémi <54523307+noemiboroka@users.noreply.github.com>
2021-10-29 16:38:33 +0200

    basics of user login and user page

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+app.secret_key = b'\xcb\xd2\x97\xd3\xbb\x86\x88$:\xc3G\x85'
+
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'USER' and password == 'PASSWORD':
+            session['user_id'] = 1
+            return redirect(url_for('user_page'))
+        else:
+            return redirect(url_for('login'))
     return render_template("login.html")
+
+@app.route("/user_page")
+def user_page():
+    return render_template("user_page.html")
 
 
 if __name__ == '__main__':
