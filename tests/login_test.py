80cfefbfd614076ebc03d107bf536c1966e10271
Tóth-Vargics Noémi <54523307+noemiboroka@users.noreply.github.com>
2021-11-12 16:00:23 +0100

    Update login_test.py

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,14 +1,14 @@
 import unittest
-import spamnoot.common
+from spamnoot.common import can_user_login
 
 
 class LoginTest(unittest.TestCase):
 
     def test_case_sensitivity(self):
         test_users = {"USER": "PASSWORD", "user": "asdasd", "Alice": "PASSWORD"}
-        self.assertEqual(common.can_user_login("USER", "password", test_users), False)
-        self.assertEqual(common.can_user_login("alice", "password", test_users), False)
-        self.assertEqual(common.can_user_login("USER", "ASDASD", test_users), False)
+        self.assertEqual(can_user_login("USER", "password", test_users), False)
+        self.assertEqual(can_user_login("alice", "password", test_users), False)
+        self.assertEqual(can_user_login("USER", "ASDASD", test_users), False)
 
 
 if __name__ == '__main__':
