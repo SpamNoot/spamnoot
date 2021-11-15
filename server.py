d360a7144cca5ba41b96cf930970be24bc7ef9ea
Sirak Balazs <hvyaid@inf.elte.hu>
2021-11-15 19:21:34 +0100

    register page added

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
+import os
 from flask import Flask, render_template, request, session, redirect, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
 import common
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+import db
+from db import get_db
+db.init_app(app)
+
+app.config.from_mapping(
+    SECRET_KEY=b'\xcb\xd2\x97\xd3\xbb\x86\x88$:\xc3G\x85',
+    DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+)
+app.config.from_pyfile('config.py', silent=True)
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
 app.secret_key = b'\xcb\xd2\x97\xd3\xbb\x86\x88$:\xc3G\x85'
-
-users = {"USER": "PAASWORD", "user":  "asdasd", "Alice": "PASSWORD"}
+app.config['DATABASE']=os.path.join(app.instance_path, 'db.sqlite')
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -13,14 +27,49 @@ def login():
         session.pop('user_id', None)
         username = request.form['username']
         password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
-        if common.can_user_login(username,password, users):
-            session['user_id'] = 1
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
             return redirect(url_for('user_page', username=username))
 
         else:
             return redirect(url_for('login'))
     return render_template("login.html")
+@app.route("/register.html", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        if not username:
+            error = 'Username is required.'
+        if not password:
+            error = 'Password is required.'
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} already exists."
+            else:
+                return redirect(url_for('login'))
+    return render_template('register.html')
+
+
 
 
 @app.route("/<username>")
@@ -34,5 +83,3 @@ if __name__ == '__main__':
         port=8000,
         debug=True,
     )
-
-#git_push_test
